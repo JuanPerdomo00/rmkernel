5b9e59e3792dbcc0f7a8f53cf660773c376a2b8a
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2023-11-02 14:05:25 -0500

    add install

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+if [[ $UID -ne 0 ]]; then 
+    echo -e "\rUse: sudo"
+fi
+
+clear 
+
+
+echo "installing in the path /usr/bin/"
+sleep 1
+cp ./rmkernel.sh /usr/bin/rmkernel
+echo "[ok] Installation successful"
+sleep 1
+echo -e "\rUse: rmkernel -h for more info
+If you find an error you can report it at: 
+"
+
+
