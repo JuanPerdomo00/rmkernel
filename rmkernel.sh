e8fd5eadc9cf13e2c54b34b77816ece532f9a8e4
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2023-11-02 14:15:18 -0500

    adding improvement

--- rmkernel.sh
+++ rmkernel.sh
@@ -34,7 +34,6 @@ if [[ $UID -ne 0 ]]; then
 fi
 
 
-
 # path temp to list kernels and creatr form rute 
 tmp_list_kernel_path=/tmp/list_kernel.txt
 touch $tmp_list_kernel_path 2>/dev/null
@@ -46,9 +45,11 @@ args2=$2
 n_args=$#
 
 function list_kernel() {
-    echo -e "$gree[+] Listing kernels$re"
+    echo -e "\r$green[+]$re Listing kernels\n"
     rpm -qa kernel\* | sort -V > $tmp_list_kernel_path
+    echo "============================================="
     cat $tmp_list_kernel_path
+    echo "============================================="
     exit 0
 }
 
@@ -70,17 +71,17 @@ function delete_kernel() {
 
     for list in $(cat $tmp_list_kernel_path); do
         if [ $args2 == $list ]; then
-            echo -e "\r$green Removing:$re $list"
+            echo -e "\r$green [+] Removing:$re $list"
             sleep 1
             sudo dnf remove $list -y >/dev/null
             clear 
-            echo -e "$green He was removed:$re $args2"
+            echo -e "\r$green[+] He was removed:$re $args2"
             rm -r --force $tmp_list_kernel_path
             exit 0
         fi
     done
 
-    echo -e "$red Kernel not found, possibly does not exist or has been uninstalled $re"
+    echo -e "\r$red[!] Kernel not found, possibly does not exist or has been uninstalled $re"
     exit 1
 }
 
@@ -88,7 +89,7 @@ function delete_kernel() {
 function grep_kenel_list() {
     # check if you have anything args2
     if [ -z $args2 ]; then
-        echo -e "\r$red You must have the kernel you want to search for.$re"
+        echo -e "\r$red[!] You must have the kernel you want to search for.$re"
         exit 1
     fi
 
@@ -101,17 +102,16 @@ function grep_kenel_list() {
         fi
     done
             
-    echo -e "\r$red\rThis kernel $green $args2$re $red no exits
-    \ror it may be uninstalled.$re
-    "
+    echo -e "\r$red\r[!] This kernel $green $args2$re $red no exits
+    \ror it may be uninstalled.$re"
     exit 1
-}
+} 
 
 
 
 function main() {
     use="\n
-    \rfor more information use $green--help $re or $green -h $re"
+    \r[?] for more information use $green--help $re or $green -h $re"
 
     if [[ $n_args -eq 0 ]]; then
         echo -e $use
@@ -130,25 +130,25 @@ function main() {
 
         \r-lk,  --list-kernels        Lists the kernels installed on your system
 
-        \r-sk,  --search-kernel       Search a kernel from list, -sk [kernel-...]      
-        
-        \r-rk   --remove-kernel       Delete a kernel specified by args, -rk [kernel...]
+        \r-sk,  --search-kernel       Search a kernel from list, '-sk [kernel-...]'      
+                                      
+        \r-rk   --remove-kernel       Delete a kernel specified by args, '-rk [kernel...]'
 
         \r-h,   --help                Print the help and exit successfully [0]
 
-        \r-v,  --version              Prints the version of the script and has an output [0]
-        "
+        \r-v,  --version              Prints the version of the script and has an output [0]"
         exit 0
     fi
 
 
     if [ $args1 == "-v" -o $args1 == "--version" ]; then
         echo -e "\rrmkernel 1.0
+
         \rCopyright (C) 2023.
         \rLicense GPLv3 GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.
         \rThis is free software: you are free to change and redistribute it.
 
-        \rWritten by: Jakepy perdomo"
+        \rWritten by:$green Jakepy perdomo$re"
         
         exit 0
 
@@ -164,16 +164,6 @@ function main() {
 }
 
 main
-
-
-
-
-
-
-
-
-
-
 
 
 
